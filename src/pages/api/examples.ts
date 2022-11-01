dad797788f34c0a2a58d6667425a4d9c682fcf08
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-11-01 17:35:17 +0700

    docs: ✏️ update schema

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -3,8 +3,14 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  if (req.method === 'POST') {
+   
+  } else {
+    // const examples = await prisma.example.findMany();
+    // res.status(200).json(examples);
+    
+    res.status(200).json({ message: 'Hello World' });
+  }
 };
 
 export default examples;
