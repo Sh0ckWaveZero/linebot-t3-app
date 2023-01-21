73712afc9367a801fe8c609aab5efabb8abc2f19
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-01-21 17:05:20 +0700

    feat: 🎸 first commit

---
+++ src/server/api/routers/account.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
+
+export const accountRouter = createTRPCRouter({
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.account.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
